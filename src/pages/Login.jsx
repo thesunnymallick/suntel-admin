658c7d873dc2fc19d54162d8016fe1b32ebf55d6
thesunnymallick <alfesunnymallick800@gmail.com>
2024-07-09 21:07:53 +0530

    update 09-07-24

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,72 @@
-import React from "react";
-import { Input } from "antd";
+import React, { useState } from "react";
+import { Input, message } from "antd";
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 import { FaRegUser } from "react-icons/fa";
 import { RiLockPasswordLine } from "react-icons/ri";
 import loginBackground from "../assets/loginBG.png";
 import logo from "../assets/logo.png";
 import { FiLogIn } from "react-icons/fi";
-
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import apiService from "../utils/apiService";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+// Define the validation schema
+const loginSchema = Yup.object({
+  email: Yup.string().email("Invalid email address").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
 const Login = () => {
+  // Login data
+  const loginData={
+    email:"",
+    password :"",
+  }
+
+  const [loginInfo, setLoginInfo]=useState(null);
+  const navigate=useNavigate();
+
+
+    const loginHandel=async(formData)=>{
+    try {
+      const {data, status}= await apiService.post(`/api/v1/seller/login`, formData);
+      console.log(data, status);
+
+      if(status===200){
+        Cookies.set("token", data?.token);
+        setLoginInfo(data?.customer);
+        message.success("Login successfull!");
+        navigate('/');
+
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+    }
+
+
+
+
+
+
+   // handel input field
+   const { handleChange, values, errors, touched, handleBlur, handleSubmit } =
+   useFormik({
+     initialValues: loginData,
+     validationSchema: loginSchema,
+     onSubmit: (values) => {
+       if (values) {
+         loginHandel(values)
+       }
+     },
+   });
+
+
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gray-50">
       <div className=" bg-white flex gap-8 w-[70%] rounded-md shadow-sm">
+
         <div className="w-[60%] flex justify-center bg-zinc-100">
           <img
             className="w-[90%] object-cover"
@@ -30,16 +86,25 @@ const Login = () => {
             </p>
           </div>
 
-          <form className="px-6 pt-4">
+          <form onSubmit={handleSubmit} className="px-6 pt-4">
             <div className="flex flex-col py-1">
               <label className="text-zinc-700 font-medium py-1 required-label">
-                Username
+                Email
               </label>
               <Input
                 size="large"
-                placeholder="Enter your user"
+                placeholder="Enter your Email"
                 prefix={<FaRegUser className="text-zinc-500" />}
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                status={touched.email && errors.email ? "error" : ""}
+
               />
+              {touched.email && errors.email ? (
+                <span className="text-red-500 text-sm">{errors.email}</span>
+              ) : null}
             </div>
 
             <div className="flex flex-col py-1">
@@ -49,12 +114,22 @@ const Login = () => {
               <Input.Password
               prefix={<RiLockPasswordLine className="text-zinc-500"/>}
               size="large"
-                placeholder="Enter your password"
-                iconRender={(visible) =>
+              placeholder="Enter your password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              status={touched.password && errors.password? "error" :""}
+              iconRender={(visible) =>
                   visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
                 }
               />
+               {touched.password && errors.password ? (
+                <span className="text-red-500 text-sm">{errors.password}</span>
+              ) : null}
             </div>
+
+
             <spna className="text-blue-500 text-sm cursor-pointer float-right font-semibold py-1" >forgot password?</spna>
 
             <button className="w-[50%] h-10 bg-blue-500 text-white rounded-sm flex 
@@ -64,6 +139,7 @@ const Login = () => {
             </button>
           </form>
         </div>
+
       </div>
     </div>
   );
---
+++ src/utils/apiService.js
@@ -0,0 +1,50 @@
+// apiService.js
+import { message } from 'antd';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+
+const url = 'http://127.0.0.1:8000';
+
+
+const apiService = axios.create({
+  baseURL: url, 
+});
+
+
+
+
+apiService.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+});
+
+
+
+// Logout User 
+apiService.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+        Cookies.remove('token'); 
+        message.error('Session has expired. Please log in again.');
+        window.location.href = '/login';
+    }
+    if(error.response && error.response.status===500){
+      message.error("Internal Server Error")
+    }
+    return Promise.reject(error);
+  }
+);
+
+
+  export default apiService;
